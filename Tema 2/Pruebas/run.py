f5454302ee3f6ecc4ca11c2a4a2bade55d26430a
nestorss05 <nestor.sanchez@iesnervion.es>
2024-11-18 10:16:55 +0100

    Update run.py

--- Tema 2/Pruebas/run.py
+++ Tema 2/Pruebas/run.py
@@ -1,28 +1,47 @@
-from time import sleep
-from multiprocessing import Process, Queue
+from random import randint
+from multiprocessing import Process, Pipe
 
-def producer(q):
-    for i in range(5):
-        q.put(i)
-        print(i, 'Enviado')
-        sleep(2)
-    q.put(None)
+def pideNumero():
+    num = int(input('Ingrese un numero: '))
+    return num
 
-def consumer(q):
+def sorteo(pipe):
+    lista=[]
     while True:
-        item = q.get()
-        if item is None:
+        if pipe.recv()==0:
+            numero = randint(1, 100)
+            while numero in lista:
+                lista.append(numero)
+            print(f"Sorteing... {numero}")
+            pipe.send(numero)
+        else:
             break
-        print(item, 'Recibido')
-    sleep(1)
+
+def jugador(pipe, num):
+    creditos = 10
+    print("Nummer: ", num)
+    while creditos > 0:
+        pipe.send(0)
+        creditos-=1
+        elegido = pipe.recv()
+        if int(elegido)==num:
+            print("GG")
+            pipe.send(1)
+            break
+        else:
+            print("PUTA")
+    if creditos == 0:
+        print("BUAAAHHH BUAAAHHHH")
+        pipe.send(1)
+    pipe.close()
 
 if __name__ == '__main__':
-    queue = Queue()
-    proceso1 = Process(target=producer, args=(queue,))
-    proceso2 = Process(target=consumer, args=(queue,))
-    proceso1.start()
-    proceso2.start()
-    proceso1.join()
-    print('Proceso 1 terminado')
-    proceso2.join()
-    print('Finiquitao')
+    numero = pideNumero()
+    pipe1, pipe2 = Pipe()
+    p1 = Process(target=sorteo, args=(pipe1,))
+    p2 = Process(target=jugador, args=(pipe2,numero))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+    print('Finiquitao final')
